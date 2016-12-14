95cb8d205ebf046c7d0688eb8a9c2b7a103dd499
Jim Clouse <jclouse@glgroup.com>
2016-12-14 20:10:06 +0000

    initial commit

---
+++ SearchCleanser/StripChars.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Data;
+using System.Text;
+using System.Globalization;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction(IsDeterministic = true, IsPrecise = true)]
+    public static string StripChars(string text)
+    {
+        var normalizedString = text.Normalize(NormalizationForm.FormD);
+        var stringBuilder = new StringBuilder();
+
+        foreach (var c in normalizedString)
+        {
+            var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+            if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+            {
+                stringBuilder.Append(c);
+            }
+        }
+
+        return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+    }
+}
