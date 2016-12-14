e61dc5ceea95239c79ea8dec2d01138e1f0b6aae
Jim Clouse <jclouse@glgroup.com>
2016-12-14 21:21:29 +0000

    updated to strip out all non alphanumeric and special case chars

--- SearchCleanser/StripChars.cs
+++ SearchCleanser/StripChars.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Data;
 using System.Text;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
@@ -11,13 +12,23 @@ public partial class UserDefinedFunctions
     [Microsoft.SqlServer.Server.SqlFunction(IsDeterministic = true, IsPrecise = true)]
     public static string StripChars(string text)
     {
+        // start by trimming leading & trailing whitespace
+        text = text.Trim();
+        
+        // loop through characters and remove diacritics or handle replacements
         var normalizedString = text.Normalize(NormalizationForm.FormD);
         var stringBuilder = new StringBuilder();
+
+        List<char> allowedChars = new List<char> { ' ', '&', '.' };
 
         foreach (var c in normalizedString)
         {
             var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
-            if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+            if (unicodeCategory == UnicodeCategory.LetterNumber ||
+                unicodeCategory == UnicodeCategory.LowercaseLetter ||
+                unicodeCategory == UnicodeCategory.UppercaseLetter ||
+               allowedChars.Contains(c)
+                )
             {
                 stringBuilder.Append(c);
             }
